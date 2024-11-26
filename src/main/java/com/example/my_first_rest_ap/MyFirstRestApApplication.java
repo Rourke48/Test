97904660654f3c4c97c555c89d96725f2bd19ba4
A304979 <Rourke48>
2024-11-26 09:01:50 +0100

    This is my first commit

---
+++ src/main/java/com/example/my_first_rest_ap/MyFirstRestApApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_first_rest_ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstRestApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstRestApApplication.class, args);
+	}
+
+}
